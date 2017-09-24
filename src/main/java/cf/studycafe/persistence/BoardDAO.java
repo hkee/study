59a2bc68d4b3fc073e0b5dfe7d6642e284dd5524
HKEEE\hyunkyung <hk041280@gmail.com>
2017-09-24 17:00:40 +0900

    mybatis-config.xml수정, boardMapper.xml수정 root-context.xml수정 BoardDAO 생성 BoardDAOTest생성

---
+++ src/main/java/cf/studycafe/persistence/BoardDAO.java
@@ -0,0 +1,24 @@
+package cf.studycafe.persistence;
+
+//import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+import cf.studycafe.domain.BoardVO;;
+
+@Repository
+public class BoardDAO {
+
+	@Inject
+	private SqlSession session;
+	
+	private static String namespace="cf.studycafe.mapper.BoardMapper";
+	
+	
+	public void create(BoardVO vo)throws Exception{
+		session.insert(namespace+".create",vo);
+	}
+	
+}
